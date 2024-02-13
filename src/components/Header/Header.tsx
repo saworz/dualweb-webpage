e806f23ef17500488a684bc0c2b3570d50e1e8aa
jedrzejbor <jedrzej.borakiewicz@gmail.com>
2024-02-13 22:40:22 +0100

    DW-8 add deviceSize settings, styled header for desktop

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import HeaderBackground from "../../assets/header-background.png"
-import LogoDW from "../../assets/logo.png"
+import HeaderBackground from "../../assets/header-background.png";
+import LogoDW from "../../assets/logo.png";
+import { device } from '../../settings/deviceSize';
 
 const Background = styled.div`
   height: 100vh;
@@ -10,19 +11,27 @@ const Background = styled.div`
   justify-content: center;
   align-items: center;
   background:no-repeat url(${HeaderBackground});
-  background-size: cover;
+  background-size: 100% 100%;
 `
 const ContentWrapper = styled.div`
+  max-width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  color: #fff;  
+  color: #fff;
 `
 
 const Text = styled.h1`
+  width: 80%;
   color: #888888;
   font-family: Open-sans, sans-serif;
-  font-size: 36;
+  font-size: 24px;
+  text-align: center;
+  
+  @media ${device.laptop} {
+    width: auto;
+    font-size: 36px;
+  }
 `
 
 const Header: React.FC = () => {
@@ -30,7 +39,7 @@ const Header: React.FC = () => {
   return (
     <Background>
       <ContentWrapper>
-        <img src={LogoDW} />
+        <img src={LogoDW} alt="dualweb logo" />
         <Text>Programujemy, Wdrażamy, Udoskonalamy</Text>
       </ContentWrapper>
     </Background>

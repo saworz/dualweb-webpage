ec07d04693a97d2747283ec33461bc29e2ab7263
jedrzejbor <jedrzej.borakiewicz@gmail.com>
2024-02-13 08:57:48 +0100

    Dw-2 add header and about us section

---
+++ src/components/AboutUs/AboutUs.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const Background = styled.div`
+  height: 100vh;
+  width: 100%;
+  background: red;
+`
+
+const AboutUs: React.FC = () => {
+
+  return (
+    <>
+      <Background>TEST2</Background>
+    </>
+  );
+}
+
+export default AboutUs;

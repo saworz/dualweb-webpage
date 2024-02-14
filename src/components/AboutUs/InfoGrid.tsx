6417434e42747bd789c3f6630fafa8b69fb032e3
Michal Cz <michal.czechowicz7@gmail.com>
2024-02-14 23:49:31 +0100

    Changed tsconfig

--- src/components/AboutUs/InfoGrid.tsx
+++ src/components/AboutUs/InfoGrid.tsx
@@ -3,8 +3,8 @@ import styled from "styled-components";
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: 50% 50%; /* Two columns, each 50% */
-  grid-template-rows: auto auto; /* Two rows */
+  grid-template-columns: 50% 50%;
+  grid-template-rows: auto auto;
   height: 100%;
   width: 100%;
 `;

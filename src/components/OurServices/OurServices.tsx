0decbbecdff69b8ab33a844d14514464fe0ef2cc
Michal Cz <michal.czechowicz7@gmail.com>
2024-02-19 17:59:32 +0100

    Added CardsContainer placement

--- src/components/OurServices/OurServices.tsx
+++ src/components/OurServices/OurServices.tsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 import Pipeline from "../PipelineBackground/Pipeline";
+import useIsInViewport from "../../hooks/isInViewport";
 
 const Background = styled.div`
+  position: relative;
   height: 100vh;
   width: 100%;
 `;
 
+const CardsContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  width: 60%;
+  height: 60%;
+  background-color: grey;
+  opacity: 0.2;
+`;
 
 const OurServices: React.FC = () => {
+  const pipelineRef = useRef(null);
+
   return (
-    // <Background>
-      <Pipeline animationSeconds={10} slowDownSteps={10}/>
-    // </Background>
+    <Background>
+      <Pipeline animationSeconds={10} slowDownSteps={10} />
+      <CardsContainer />
+    </Background>
   )
 }
 

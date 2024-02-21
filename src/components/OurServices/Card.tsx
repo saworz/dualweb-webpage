fc6ca2e6f4b894508735b8dffbdc397500c00de0
Michal Cz <michal.czechowicz7@gmail.com>
2024-02-22 00:06:18 +0100

    Created services cards

--- src/components/OurServices/Card.tsx
+++ src/components/OurServices/Card.tsx
@@ -1,36 +1,56 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import styled from 'styled-components';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import styled from "styled-components";
 
+const ImageContainer = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+  height: 100px;
+`;
 
-const MediaCard = () => {
+const CardContainer = styled.div`
+    opacity: 0.85;
+    width: 260px;
+    height: 400px;
+    margin: 3vh;
+    background-color: white;
+    border-radius: 10px;
+`;
+const Image = styled.img`
+  max-width: 70px;
+`;
+
+const MediaCard = ({
+  title,
+  description,
+  image,
+}: {
+  title: string;
+  description: string;
+  image: string;
+}) => {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
-        title="green iguana"
-      />
+    <CardContainer>
+      <ImageContainer>
+        <Image src={image} />
+      </ImageContainer>
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          {title}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          {description}
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    </CardContainer>
+
   );
-}
+};
 
 export default MediaCard;
--- src/components/OurServices/OurServices.tsx
+++ src/components/OurServices/OurServices.tsx
@@ -3,6 +3,11 @@ import styled from "styled-components";
 import Pipeline from "../PipelineBackground/Pipeline";
 import useIsInViewport from "../../hooks/isInViewport";
 import MediaCard from "./Card";
+import ComputerIMG from "../../assets/services/computer.png";
+import WrenchIMG from "../../assets/services/wrench.png";
+import EngineerIMG from "../../assets/services/engineers.png";
+import HandshakeIMG from "../../assets/services/handshake.png";
+import ShoppingCartIMG from "../../assets/services/shopping_cart.png";
 
 const Background = styled.div`
   position: relative;
@@ -60,15 +65,40 @@ const OurServices: React.FC = () => {
       />
       <PipelineStartTrigger ref={animationStartRef} />
       <PipelineResetTriggerTop ref={animationStopTopRef} />
-      <PipelineResetTriggerBot ref={animationStopBotRef} /> 
+      <PipelineResetTriggerBot ref={animationStopBotRef} />
 
       <CardsContainer>
-        <MediaCard />
-        <MediaCard />
-        <MediaCard />
-        <MediaCard />
+        <MediaCard
+          title="Strony www i aplikacje internetowe"
+          description="Realizujemy projekty oparte na nowoczesnych technologiach, 
+          gwarantując interaktywne i atrakcyjne rozwiązania, dostosowane do dynamicznie zmieniającego się środowiska online."
+          image={ComputerIMG}
+        />
+        <MediaCard
+          title="Rozbudowa systemów"
+          description="Działamy na rzecz naszych klientów, dostosowując systemy do nowych wymagań i standardów branżowych. 
+          Przeprowadzamy analizę istniejących rozwiązań, identyfikując obszary poprawy i proponując skuteczne strategie rozbudowy."
+          image={WrenchIMG}
+        />
+        <MediaCard
+          title="Doświadczony zespół"
+          description="Rozumiemy unikalne potrzeby i wymagania co pozwala nam dostarczyć rozwiązania odpowiadające najwyższym standardom 
+          jakości. Współpracujemy harmonijnie, wykorzystując kreatywność i zdolności techniczne, aby tworzyć innowacyjne i skuteczne produkty."
+          image={EngineerIMG}
+        />
+        <MediaCard
+          title="Sklepy internetowe"
+          description="Realizujemy projekty sklepów internetowych zapewniając intuicyjne i bezproblemowe doświadczenie zakupowe dla klientów. Nasze podejście opiera się na 
+          wykorzystaniu najnowszych technologii e-commerce, optymalizacji procesów płatności oraz integracji z systemami logistycznymi."
+          image={ShoppingCartIMG}
+        />
+        <MediaCard
+          title="Wsparcie techniczne"
+          description=" Oferujemy zarówno wsparcie online, jak i telefoniczne, zapewniając klientom szybki dostęp do niezbędnej pomocy. Nasza obsługa techniczna obejmuje 
+          monitorowanie wydajności systemów, aktualizacje oprogramowania oraz naprawy awarii."
+          image={HandshakeIMG}
+        />
       </CardsContainer>
-
     </Background>
   );
 };

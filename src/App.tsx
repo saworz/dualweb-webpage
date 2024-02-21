9c2c5fe99333dae41953e848d1965f9e3b4e1572
jedrzejbor <jedrzej.borakiewicz@gmail.com>
2024-02-21 05:49:31 +0100

    DW-8 clean app

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,16 @@ import Header from "./components/Header/Header";
 import AboutUs from "./components/AboutUs/AboutUs";
 import ContactUs from "./components/ContactUs/ContactUs";
 import OurServices from "./components/OurServices/OurServices";
-import Pipeline from "./components/PipelineBackground/Pipeline";
 import "./index.css";
 
-// class App extends Component {
-//   render() {
-//       return (
-//           <div className="App">
-//               <Pipeline animationSeconds={10} slowDownSteps={5}/>
-//               <h1>Hello, World!</h1>
-//           </div>
-//       );
-//   }
-// }
 
 const App: React.FC = () => {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <AboutUs />
       <OurServices />
-       
+      <ContactUs />
     </>
   );
 };

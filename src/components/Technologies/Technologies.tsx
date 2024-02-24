28956b5ab3e6c032c14c44f6cb4df655abfb3e57
Michal Cz <michal.czechowicz7@gmail.com>
2024-02-25 00:38:22 +0100

    Changed background

--- src/components/Technologies/Technologies.tsx
+++ src/components/Technologies/Technologies.tsx
@@ -1,9 +1,19 @@
 import styled from "styled-components";
-import { BackgroundBeamsDemo } from "../BeamsBackground/BackgroundBeams";
+import BackgroundIMG from "../../assets/laptop.jpeg"
+
+const Background = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background:no-repeat url(${BackgroundIMG});
+  background-size: 100% 100%;
+`
 
 const Technologies = () => {
   return (
-    <BackgroundBeamsDemo />            
+    <Background />        
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,3 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
- 
-const colors = require("tailwindcss/colors");
-const {
-  default: flattenColorPalette,
-} = require("tailwindcss/lib/util/flattenColorPalette");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
@@ -23,16 +16,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/aspect-ratio"), addVariablesForColors],
-}
-
-function addVariablesForColors({ addBase, theme }) {
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  );
- 
-  addBase({
-    ":root": newVars,
-  });
+  plugins: [],
 }
